dd34fef4b0e713c9b23a1d9d8f810ed1a333b010
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-07-26 17:33:15 +0500

    db uzgardi

--- config/settings.py
+++ config/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default':env.dj_db_url("DATABASE_URL")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default':env.dj_db_url("DATABASE_URL")
 }
 
 
